9b418d02acd9e85d6fc834e17fe780a677f908d9
s-nt-s <santos82h@gmail.com>
2020-02-15 19:46:06 +0100

    nuevos alias, y pront simplificado

--- files/etc/profile.d/prompt.sh
+++ files/etc/profile.d/prompt.sh
@@ -1,14 +1,14 @@
-SCREEN_SESSION_NAME=$(screen -ls | sed -n 's|[^a-z0-9]*\([0-9][0-9]*\.[a-zA-Z][a-zA-Z0-9-]*\).*Attached.*|\1|p')
+SCREEN_SESSION_NAME=""
 
-if [ ! -z "$SCREEN_SESSION_NAME" ]; then
-   SCREEN_SESSION_NAME="$SCREEN_SESSION_NAME "
+if [ ! -z "$STY" ]; then
+  MY_HOSTNAME=$(uci get system.@system[0].hostname)
+  SCREEN_SESSION_NAME=$(echo "${STY}" | sed "s|\.${MY_HOSTNAME}$||")" "
 fi
 
 export PS1='${SCREEN_SESSION_NAME}\w \$ '
 
 case "$TERM" in
-        xterm*|rxvt*|screen)
-                export PS1='${SCREEN_SESSION_NAME}\[\e[1;31m\]\w \$ \[\e[0m\]'
-                ;;
+  xterm*|rxvt*|screen)
+    export PS1='${SCREEN_SESSION_NAME}\[\e[1;31m\]\w \$ \[\e[0m\]'
+  ;;
 esac
-
