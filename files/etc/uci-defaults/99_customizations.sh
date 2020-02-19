2ba58d67a6d888b512d9897d79aabbc3fe3a34c1
s-nt-s <santos82h@gmail.com>
2020-02-19 21:11:44 +0100

    puntos de montaje

--- files/etc/uci-defaults/99_customizations.sh
+++ files/etc/uci-defaults/99_customizations.sh
@@ -9,15 +9,15 @@ rm /etc/banner
 if [ -f /etc/config/fstab ]; then
 if ! grep -q '/dev/sda1' /etc/config/fstab; then
 cat << EOF_cat >> /etc/config/fstab
-config swap
-	option device '/dev/sda2'
-	option enabled '1'
-
 config mount
 	option device '/dev/sda1'
 	option enabled '1'
 	option target '/mnt/usb'
 	option enabled_fsck '1'
+
+config swap
+	option device '/dev/sda2'
+	option enabled '1'
 EOF_cat
 else
   sed "s/option\s*enabled\s*'0'/option\tenabled\t'1'/g" -i /etc/config/fstab
