afe9b8631961081b23271306d94e1aff451fa6e8
s-nt-s <santos82h@gmail.com>
2020-02-20 20:08:05 +0100

    nueva forma de obtener la ip pública

--- files/usr/bin/getip.sh
+++ files/usr/bin/getip.sh
@@ -1,12 +1,36 @@
 #!/bin/sh
+DBG="$1"
+
+MY_DIG=""
+
+if command -v dig >/dev/null 2>&1; then
+  MY_DIG="dig"
+fi
+if command -v kdig >/dev/null 2>&1; then
+  MY_DIG="kdig"
+fi
+
+if [ ! -z "$MY_DIG" ]; then
+  if [ "$DBG" == "-v" ]; then
+    "$MY_DIG" +short myip.opendns.com @resolver1.opendns.com
+  else
+    "$MY_DIG" +short myip.opendns.com @resolver1.opendns.com 2>/dev/null
+  fi
+  exit $?
+fi
 
 getIp () {
-   IP=$(wget -q -T 4 "$1" -O -)
+   IP=$(wget -q -T 4 "http://$1" -O -)
    if [ ! -z "$IP" ] && [ ${#IP} -ge 8 ] && [ ${#IP} -le 16 ]; then
       echo "$IP"
       exit 0
    fi
+   if [ "$DBG" == "-v" ]; then
+      wget "http://$1" -O -
+   fi
 }
-getIp "http://ifconfig.me"
-getIp "http://icanhazip.com"
+getIp ifconfig.me
+getIp icanhazip.com
+getIp ipecho.net/plain
+getIp ifconfig.co
 exit 1
